2957d3b49952d64067467d1fce4ff78f3e11bcc4
Kell <maybe.hello.world@gmail.com>
2019-04-09 14:26:07 +0300

    non-valuable changes

--- grid_usage.py
+++ grid_usage.py
@@ -3,32 +3,36 @@
 from qfac import gridsearch_creator
 
 _common_opts = {
-	"OVERALL_STEPS": [70000]
+	"OVERALL_STEPS": [600000]
 }
 
 _kfac_opts = {
-	'learning_rate': [1e-4, 1e-3, 3e-3],
-	'cov_ema_decay': [0.80, 0.9, 0.95],
-	'momentum': [0.3, 0.5, 0.7],
-	'damping': [3e-1, 3e-2, 3e-3]
+	'learning_rate': [1e-4, 5e-4, 1e-3],
+	'cov_ema_decay': [0.95],
+	'momentum': [0.9],
+	'damping': [0.01, 0.05]
 }
 
 _adam_opts = {
-	'learning_rate': [5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1]
+	'learning_rate': [1e-3],
+	'beta1': [0.5, 0.9],
+	'beta2': [0.99, 0.999],
+	'epsilon': [1e-08]
 }
 
 _rmsprop_opts = {
-	'learning_rate': [1e-4, 25e-5, 5e-4, 1e-3],
-	'decay': [0.9, 0.95, 0.99],
-	'momentum': [0.9, 0.95, 0.95],
+	'learning_rate': [1e-4, 1e-3],
+	'decay': [0.95, 0.99],
+	'momentum': [0.95, 0.99],
 }
 
 gridsearch_creator.create_grid(
-	_optimizer="rmsprop",
+	_optimizer="adam",
 	filename="task",
 	n_hosts=4,
 	n_gpus=2,
-	_optimizer_opts=_rmsprop_opts,
+	_gym_env=['Breakout-v0'],
+	_optimizer_opts=_adam_opts,
 	_common_opts=_common_opts,
-	_seed=[1, 2, 3, 4]
+	_seed=[i for i in range(20)]
 )
